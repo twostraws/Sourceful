9af929a7b757121e1c24e34c8360887f41d37059
Taylor Holliday <taylor@audulus.com>
2021-09-18 12:33:40 -0700

    fix #25 . Don't update state if the text hasn't changed.

--- Sources/Sourceful/View/SyntaxTextView.swift
+++ Sources/Sourceful/View/SyntaxTextView.swift
@@ -290,15 +290,19 @@ open class SyntaxTextView: _View {
         }
         set {
             #if os(macOS)
-            textView.layer?.isOpaque = true
-            textView.string = newValue
-            refreshColors()
+            if textView.string != newValue {
+                textView.layer?.isOpaque = true
+                textView.string = newValue
+                refreshColors()
+            }
             #else
             // If the user sets this property as soon as they create the view, we get a strange UIKit bug where the text often misses a final line in some Dynamic Type configurations. The text isn't actually missing: if you background the app then foreground it the text reappears just fine, so there's some sort of drawing sync problem. A simple fix for this is to give UIKit a tiny bit of time to create all its data before we trigger the update, so we push the updating work to the runloop.
             DispatchQueue.main.async {
-                self.textView.text = newValue
-                self.textView.setNeedsDisplay()
-                self.refreshColors()
+                if self.textView.text != newValue {
+                    self.textView.text = newValue
+                    self.textView.setNeedsDisplay()
+                    self.refreshColors()
+                }
             }
             #endif
 
