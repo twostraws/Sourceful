82a2932dee293ec86b26aef86cd86692524bb118
Frederik Fraaz <f.fraaz@googlemail.com>
2023-08-22 18:04:54 +0200

    Fix Python3Lexer .identifier regex

--- Sources/Sourceful/Languages/Python3Lexer.swift
+++ Sources/Sourceful/Languages/Python3Lexer.swift
@@ -23,7 +23,7 @@ public class Python3Lexer: SourceCodeRegexLexer {
 		
 		generators.append(regexGenerator("(?<=[^a-zA-Z])\\d+", tokenType: .number))
 		
-		generators.append(regexGenerator("\\.\\w+", tokenType: .identifier))
+		generators.append(regexGenerator("(?<=\\.)[A-Za-z_]+\\w*", tokenType: .identifier))
 		
 		let keywords = "False None True and as assert break class continue def del elif else except finally for from global if import in is lambda nonlocal not or pass raise return try while with yield".components(separatedBy: " ")
         
